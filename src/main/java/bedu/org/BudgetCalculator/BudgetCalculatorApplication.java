ad8affa6809d2e85d34f8cea415053bf732b906f
Diana Yesenia Campos Tenorio <73143272+Dyeseniact@users.noreply.github.com>
2023-11-18 10:13:14 -0700

    first commit

---
+++ src/main/java/bedu/org/BudgetCalculator/BudgetCalculatorApplication.java
@@ -0,0 +1,13 @@
+package bedu.org.BudgetCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bedu/org/BudgetCalculator/BudgetCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package bedu.org.BudgetCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
